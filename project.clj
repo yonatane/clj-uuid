e65348f37f75c85da342041f8b3701ab68a8bc9c
Dan Lentz <dan.lentz@dean.com>
2015-11-21 17:01:00 -0500

    0.2.0-SNAPSHOT
    clojure dependency => 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject danlentz/clj-uuid "0.1.7-SNAPSHOT"
+(defproject danlentz/clj-uuid "0.2.0-SNAPSHOT"
   :description  "A Clojure library for generation and utilization of
                 UUIDs (Universally Unique Identifiers) as described by
                 RFC-4122. This library extends the standard Java UUID class
@@ -11,7 +11,7 @@
   :url          "https://github.com/danlentz/clj-uuid"
   :license      {:name "Eclipse Public License"
                   :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [primitive-math      "0.1.4"]]
   :codox    {:output-dir  "doc/api"
              :src-dir-uri "https://github.com/danlentz/clj-uuid/blob/master/"
